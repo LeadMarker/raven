f636a56fb1388e3344996b10cadcff5648d8367b
afy <73753274+afyzone@users.noreply.github.com>
2024-03-10 20:03:51 -0700

    Update settings_table.lua

--- settings_table.lua
+++ settings_table.lua
@@ -7,7 +7,7 @@ getgenv().settings = {
     target_type = 'hold', -- toggle, hold
 
     triggerbot_type = 'hold', -- toggle, hold
-    esp_toggle_bind = 'K',
+    esp_toggle_bind = 'M',
 
     show_target = false,
     show_nearest_point = false,
@@ -16,8 +16,8 @@ getgenv().settings = {
     silent_settings = {
         silent_fov = {
             fov_color = Color3.fromRGB(195, 209, 38),
-            fov_radius = 300,
-            fov_visible = false
+            fov_radius = 100,
+            fov_visible = true
         },
 
         hit_chance = 100,
@@ -47,13 +47,13 @@ getgenv().settings = {
         
         realistic_mouse_movement_settings = {
             smoothing = {
-                horizontal = 6,
+                horizontal = 8,
                 vertical = 8
             },
 
             sensitivity = { -- If you don't know what to put, just put your roblox sensitivity for both
-                x_axis = 1.095,
-                y_axis = 1.095
+                x_axis = 1,
+                y_axis = 1
             },
         },
 
@@ -63,12 +63,12 @@ getgenv().settings = {
                 second = 'Bounce'
             },
 
-            sensitivity = 0.20015
+            sensitivity = 0.2
         },
 
         cam_fov = {
             fov_color = Color3.fromRGB(90, 50, 168),
-            fov_radius = 300,
+            fov_radius = 100,
             fov_visible = false
         },
 
@@ -87,29 +87,29 @@ getgenv().settings = {
         thug_cam_shake = { -- 1 is default, the higher number the more thug shake
             enabled = false,
 
-            shake_x = 15,
-            shake_y = 15,
-            shake_z = 15
+            shake_x = 1,
+            shake_y = 1,
+            shake_z = 1
         }
     },
 
     ping_settings = {
-        prediction_amount = 0.14073,
+        prediction_amount = 0.1,
         
         use_ping_sets = true,
         ping_sets = { -- if its nil it will auto use the ones we put in
-            ping10_20 = 0.01011,
-            ping20_30 = 0.01267,
-            ping30_40 = 0.02678,
-            ping40_50 = 0.04089,
-            ping50_60 = 0.05500,
-            ping60_70 = 0.06911,
-            ping70_80 = 0.08322,
-            ping80_90 = 0.09733,
-            ping90_100 = 0.11144,
-            ping100_110 = 0.12555,
-            ping110_120 = 0.13966,
-            ping120_130 = 0.15377,
+            ping10_20 = nil,
+            ping20_30 = nil,
+            ping30_40 = nil,
+            ping40_50 = nil,
+            ping50_60 = nil,
+            ping60_70 = nil,
+            ping70_80 = nil,
+            ping80_90 = nil,
+            ping90_100 = nil,
+            ping100_110 = nil,
+            ping110_120 = nil,
+            ping120_130 = nil,
             ping130_140 = nil,
             ping140_150 = nil,
             ping150_160 = nil,
@@ -132,12 +132,12 @@ getgenv().settings = {
         cam_spin = {
             enabled = false,
             speed = 20,
-            bind = 'C'
+            bind = 'N'
         },
 
         air_shot = { -- the higher the number the closer to hrp / torso
             jumping_value = 2.8,
-            falling_value = 1
+            falling_value = 0.1
         },
 
         whitelisted_players = {
@@ -160,23 +160,23 @@ getgenv().settings = {
 
         builtin_macro = {
             enabled = true,
-            bind = 'X',
+            bind = 'B',
             mode = 'hold', -- toggle, hold
             delay = 0.010
         },
 
         triggerbot = {
             enabled = true,
-            bind = 'Z',
+            bind = 'V',
             delay = 0.02,
 
             triggerbot_fov = {
                 fov_color = Color3.fromRGB(195, 209, 38),
-                fov_smoothing = 0.1,
+                fov_smoothing = 1,
                 fov_visible = true,
 
-                fov_height = 5.5,
-                fov_width = 2.5
+                fov_height = 6,
+                fov_width = 4
             }
         },
 
@@ -189,7 +189,7 @@ getgenv().settings = {
 
         anti_lock = {
             enabled = true,
-            bind = 'Y',
+            bind = 'X',
             mode = 'toggle' -- toggle, hold
         },
 
@@ -204,7 +204,7 @@ getgenv().settings = {
             color = Color3.fromRGB(255, 0, 0)
         },
 
-        force_resolver = false
+        force_resolver = true
     }
 }
 

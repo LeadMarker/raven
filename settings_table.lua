10a41fdc501a5c27207d21840c473ae3ba6e23a2
afy <73753274+afyzone@users.noreply.github.com>
2024-03-12 00:55:10 -0700

    Update settings_table.lua

--- settings_table.lua
+++ settings_table.lua
@@ -7,7 +7,7 @@ getgenv().settings = {
     target_type = 'hold', -- toggle, hold
 
     triggerbot_type = 'hold', -- toggle, hold
-    esp_toggle_bind = 'M',
+    esp_toggle_bind = 'P',
 
     show_target = false,
     show_nearest_point = false,
@@ -16,8 +16,8 @@ getgenv().settings = {
     silent_settings = {
         silent_fov = {
             fov_color = Color3.fromRGB(195, 209, 38),
-            fov_radius = 100,
-            fov_visible = true
+            fov_radius = 300,
+            fov_visible = false
         },
 
         hit_chance = 100,
@@ -48,27 +48,27 @@ getgenv().settings = {
         realistic_mouse_movement_settings = {
             smoothing = {
                 horizontal = 8,
-                vertical = 8
+                vertical = 6
             },
 
             sensitivity = { -- If you don't know what to put, just put your roblox sensitivity for both
-                x_axis = 1,
-                y_axis = 1
+                x_axis = 0.25,
+                y_axis = 0.25
             },
         },
 
         robotic_mouse_movement = {
             movement_types = { -- Linear, Sine, Back, Quad, Quart, Quint, Bounce, Elastic, Exponential, Circular, Cubic
-                first = 'Linear',
+                first = 'Cubic',
                 second = 'Bounce'
             },
 
-            sensitivity = 0.2
+            sensitivity = 0.20015
         },
 
         cam_fov = {
             fov_color = Color3.fromRGB(90, 50, 168),
-            fov_radius = 100,
+            fov_radius = 300,
             fov_visible = false
         },
 
@@ -78,45 +78,57 @@ getgenv().settings = {
                 Empty = all parts;
                 {"HumanoidRootPart", "Head", "LeftHand", "RightHand", "LeftLowerArm", "RightLowerArm", "LeftUpperArm", "RightUpperArm", "LeftFoot", "LeftLowerLeg", "UpperTorso", "LeftUpperLeg", "RightLowerLeg", "RightFoot", "LowerTorso", "RightUpperLeg"}
             ]]
-            'Head',
             'HumanoidRootPart',
+            'Head',
+            'LeftHand',
+            'RightHand',
+            'LeftLowerArm',
+            'RightLowerArm',
+            'LeftUpperArm',
+            'RightUpperArm',
+            'LeftFoot',
+            'LeftLowerLeg',
             'UpperTorso',
-            'LowerTorso'
+            'LeftUpperLeg',
+            'RightLowerLeg',
+            'RightFoot',
+            'LowerTorso',
+            'RightUpperLeg',
         },
         
         thug_cam_shake = { -- 1 is default, the higher number the more thug shake
             enabled = false,
 
-            shake_x = 1,
-            shake_y = 1,
-            shake_z = 1
+            shake_x = 15,
+            shake_y = 15,
+            shake_z = 15
         }
     },
 
     ping_settings = {
-        prediction_amount = 0.1,
+        prediction_amount = 0.14073,
         
         use_ping_sets = true,
         ping_sets = { -- if its nil it will auto use the ones we put in
-            ping10_20 = nil,
-            ping20_30 = nil,
-            ping30_40 = nil,
-            ping40_50 = nil,
-            ping50_60 = nil,
-            ping60_70 = nil,
-            ping70_80 = nil,
-            ping80_90 = nil,
-            ping90_100 = nil,
-            ping100_110 = nil,
-            ping110_120 = nil,
-            ping120_130 = nil,
-            ping130_140 = nil,
-            ping140_150 = nil,
-            ping150_160 = nil,
-            ping160_170 = nil,
-            ping170_180 = nil,
-            ping180_190 = nil,
-            ping190_200 = nil
+            ping10_20 = 0.12588,
+            ping20_30 = 0.05500,
+            ping30_40 = 0.06911,
+            ping40_50 = 0.10017,
+            ping50_60 = 0.1229,
+            ping60_70 = 0.1229,
+            ping70_80 = 0.128,
+            ping80_90 = 0.13702,
+            ping90_100 = 0.13712,
+            ping100_110 = 0.13722,
+            ping110_120 = 0.13736,
+            ping120_130 = 0.13812,
+            ping130_140 = 0.150,
+            ping140_150 = 0.15214,
+            ping150_160 = 0.1514,
+            ping160_170 = 0.1663,
+            ping170_180 = 0.1672,
+            ping180_190 = 0.1848,
+            ping190_200 = 0.1865
         }
     },
     
@@ -132,12 +144,12 @@ getgenv().settings = {
         cam_spin = {
             enabled = false,
             speed = 20,
-            bind = 'N'
+            bind = 'C'
         },
 
         air_shot = { -- the higher the number the closer to hrp / torso
-            jumping_value = 2.8,
-            falling_value = 0.1
+            jumping_value = 3.2,
+            falling_value = 0.07
         },
 
         whitelisted_players = {
@@ -160,37 +172,39 @@ getgenv().settings = {
 
         builtin_macro = {
             enabled = true,
-            bind = 'B',
+            bind = 'x',
             mode = 'hold', -- toggle, hold
             delay = 0.010
         },
 
         triggerbot = {
             enabled = true,
-            bind = 'V',
-            delay = 0.02,
+            bind = 'z',
+            delay = 0.01,
 
             triggerbot_fov = {
                 fov_color = Color3.fromRGB(195, 209, 38),
-                fov_smoothing = 1,
+                fov_smoothing = 1.5,
                 fov_visible = true,
 
-                fov_height = 6,
-                fov_width = 4
+                fov_height = 5.5,
+                fov_width = 2.5
             }
         },
 
         raid_awareness = {
             enabled = true,
+            toggle_bind = 'C',
+            snipe_detection = true, -- if true it will automatically enable esp on players who joined you.
+
             esp_color = Color3.fromRGB(15, 23, 255),
-            friendly_color = Color3.fromRGB(0, 255, 0),
-            toggle_bind = 'C'
+            friendly_color = Color3.fromRGB(0, 255, 0)
         },
 
         anti_lock = {
             enabled = true,
-            bind = 'X',
-            mode = 'toggle' -- toggle, hold
+            bind = 'V',
+            mode = 'hold' -- toggle, hold
         },
 
         tracer_settings = {
